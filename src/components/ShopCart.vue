b0499b39780a5511fd64296d2edcde261319fcfd
cookiej <cookiejlim@gmail.com>
2018-03-08 14:56:29 +0800

    add m-cart-list component

--- src/components/ShopCart.vue
+++ src/components/ShopCart.vue
@@ -1,47 +1,24 @@
 <template>
   <div class="shop-cart">
     <m-header></m-header>
-    <div v-for="item in list" :key="item.id" class="cart-item">
-        {{ item.name }}：￥{{ item.price }} x {{ item.count }}
-      <button @click="removeFruit(item)" :disabled="deBtnDisabled(item.id)"> - </button>
-      <button @click="addFruit(item)" :disabled="inBtnDisabled(item.id)"> + </button>
-      <button @click="delFruit(item)" :disabled="deBtnDisabled(item.id)">Delete</button>
-    </div>
+    <m-cart-list></m-cart-list>
     <m-footer></m-footer>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
 import Header from './m-header';
 import Footer from './m-footer';
+import CartList from './m-cart-list';
 
 export default {
-  computed: mapGetters({
-    list: 'list',
-    inBtnDisabled: 'incrementBtnDisabled',
-    deBtnDisabled: 'decrementBtnDisabled',
-  }),
-  methods: mapActions([
-    'addFruit',
-    'removeFruit',
-    'delFruit',
-  ]),
   created() {
     this.$store.dispatch('fetchListData');
   },
   components: {
     'm-header': Header,
     'm-footer': Footer,
+    'm-cart-list': CartList,
   },
 };
 </script>
-
-<style scoped>
-  .cart-item {
-    height: 44px;
-    justify-content: space-around;
-    line-height: 44px;
-    width: 100%;
-  }
-</style>
---
+++ src/components/m-cart-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="item in list" :key="item.id" class="cart-item">
+      {{ item.name }}：￥{{ item.price }} x {{ item.count }}
+      <button @click="removeFruit(item)" :disabled="deBtnDisabled(item.id)"> - </button>
+      <button @click="addFruit(item)" :disabled="inBtnDisabled(item.id)"> + </button>
+      <button @click="delFruit(item)" :disabled="deBtnDisabled(item.id)">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: mapGetters({
+    list: 'list',
+    inBtnDisabled: 'incrementBtnDisabled',
+    deBtnDisabled: 'decrementBtnDisabled',
+  }),
+  methods: mapActions([
+    'addFruit',
+    'removeFruit',
+    'delFruit',
+  ]),
+};
+</script>
+
+<style scoped>
+  .cart-item {
+    height: 44px;
+    justify-content: space-around;
+    line-height: 44px;
+    width: 100%;
+  }
+</style>
